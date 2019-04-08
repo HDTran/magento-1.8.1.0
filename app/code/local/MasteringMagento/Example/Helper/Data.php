64b025ff27bac273c82244c5890ab6c02ec74593
Huy Tran <huytran@kiwico.com>
2019-04-08 00:37:55 -0700

    feat: Official course code snippets

--- app/code/local/MasteringMagento/Example/Helper/Data.php
+++ app/code/local/MasteringMagento/Example/Helper/Data.php
@@ -1,7 +1,16 @@
 <?php
-
-class MasteringMagento_Example_Helper_Data extends Mage_Core_Helper_Abstract {
-
+/**
+ * app/code/local/MasteringMagento/Example/Helper/Data.php
+ *
+ * This example code is provided for use with the Mastering Magento video
+ * series, by Packt Publishing.
+ *
+ * @author    Franklin P. Strube <franklin.strube@gmail.com>
+ * @category  MasteringMagento
+ * @package   Example
+ * @copyright Copyright (c) 2012 Packt Publishing (http://packtpub.com)
+ */
+class MasteringMagento_Example_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    // Empty helper class
 }
-
-?>
